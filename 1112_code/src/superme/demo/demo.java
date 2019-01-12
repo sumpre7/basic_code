c4792aec2a85ba6fb71103b681c0ff9a4920cae9
supre7me <907331074@qq.com>
2019-01-12 11:33:45 +0800

    上传版本

---
+++ 1112_code/src/superme/demo/demo.java
@@ -0,0 +1,21 @@
+package superme.demo;
+
+import java.util.Scanner;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author: hehuan
+ * Date: Created in 20:24 2018/11/12
+ */
+public class demo {
+    public static void main(String[] args) {
+        Scanner sc = null;
+        System.out.println("24332");
+        System.out.println("24332");
+        System.out.println("s4234we");
+        System.out.println("24332");
+        System.out.println("s4234we");
+
+    }
+
+}
